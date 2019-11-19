9c2de3de1a73d5d1f754c6e48ab4024f55dd0f3f
Domenico <domeniconicoli@gmail.com>
2019-11-19 21:56:09 +0100

    Initial Commit

---
+++ SiriKitIntentsUIExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SiriKitIntentsUIExample
+//
+//  Created by Domo on 19/11/2019.
+//  Copyright © 2019 Domo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
